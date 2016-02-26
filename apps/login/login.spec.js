bf4ba5d75e6e1791fa64a97d3c7bf5fd5709fb69
Patrick Tchankue <ptchankue@gmail.com>
2016-02-26 16:01:32 +0200

    Test for the login app

---
+++ apps/login/login.spec.js
@@ -0,0 +1,16 @@
+//git pull --rebase
+describe('example test', function() {
+    it('should be true', function() {
+        expect('foo').toBe('foo');
+    });
+});
+describe('sorting the list of users', function() {
+  it('sorts in descending order by default', function() {
+    var users = ['jack', 'igor', 'jeff'];
+
+    var sorted = users.sort();
+    //var sorted = fruits.reverse();
+    console.log(sorted.reverse());
+    expect(sorted).toEqual(['jeff', 'jack', 'igor']);
+  });
+});
