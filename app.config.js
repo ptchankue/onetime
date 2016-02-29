9cd80089579900ee89b4236176e89d545ac9700f
Patrick Tchankue <ptchankue@gmail.com>
2016-02-29 21:04:12 +0200

    Fixing an issue in the instanciation of app.module

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-      angular.module('app', [])
+      angular.module('app') //don't put the []
         .constant("myConfig", {
               "mode": "dev",
               "name": "OneTime",
